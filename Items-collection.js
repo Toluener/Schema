717dbf63738bfd4b2dcc4464833e2ae11196d7a5
Ehinmosan Tolu <toluehinmosan51@gmail.com>
2023-03-06 01:22:34 +0100

    created a schema for the item object

---
+++ Items-collection.js
@@ -0,0 +1,10 @@
+Item = {
+    _id: ObjectId,
+    name: "string",
+    category: ["string"],
+    description: "string",
+    price: number,
+    status: "string (available/not available)",
+    productReview: "string",
+    sellerInfo: "string"
+}
